1805690b59d598d4faee5eb2ca5f018cbbcd92c1
Mariam Tokarski <mariamtokarski@gmail.com>
2018-01-19 15:45:42 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,30 @@
+var katzDeli = []
+
+function takeANumber(katzDeliLine, name){
+  katzDeliLine.push(name)
+  return "Welcome, " + name + ". You are number " + katzDeliLine.length + " in line."
+}
+
+function nowServing(katzDeliLine){
+  if(katzDeliLine.length<1){
+    return "There is nobody waiting to be served!"
+  } else {
+    var serving = katzDeliLine.shift()
+    return "Currently serving " + "."
+  }
+}
+
+function currentLine(katzDeliLine){
+  if (katzDeliLine.length<1) {
+    return "The line is currently empty."
+  } else {
+    var katzDeli = "The line is currently:"
+
+    for (var i = 0; i < katzDeliLine.length; i++) {
+      var position = i + 1
+      var person = katzDeliLine[i]
+      katzDeli += position + ". " + person + ", "
+    }
+    return katzDeli.slice(0,katzDeliLine.length - 1)
+  }
+}
